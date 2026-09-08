19db9815706ae2868fddad091308ca960c1597e8
Christopherbizz <96215655+Christopherbizz@users.noreply.github.com>
2026-09-08 17:24:21 -0400

    Update VPC_with_EC2.tf
    Fix Security Risk.

--- VPC_with_EC2.tf
+++ VPC_with_EC2.tf
@@ -64,15 +64,15 @@ resource "aws_security_group" "demo-vpc-sg" {
   name        = "demo-vpc-sg"
   vpc_id      = aws_vpc.demo-vpc.id
    
- ingress {
-  
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
+ingress {
+
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = [var.allowed_ssh_cidr]
 
 }
+
 egress {
   
   from_port         = 0
